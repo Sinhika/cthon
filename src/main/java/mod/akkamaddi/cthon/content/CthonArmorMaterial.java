8993f03e664e205c474bbba83c56e071f996f7f8
Sinhika <sinhika@republicofnewhome.org>
2021-05-23 11:04:59 -0500

    finished specifying armor material

--- src/main/java/mod/akkamaddi/cthon/content/CthonArmorMaterial.java
+++ src/main/java/mod/akkamaddi/cthon/content/CthonArmorMaterial.java
@@ -10,6 +10,7 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import mod.akkamaddi.cthon.init.ModItems;
 
 public enum CthonArmorMaterial implements IArmorMaterial 
 {
