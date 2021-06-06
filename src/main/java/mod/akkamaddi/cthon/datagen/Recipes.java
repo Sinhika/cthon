1a4f8746b3f2f5e0895699386c5e6f93815c3183
Sinhika <sinhika@republicofnewhome.org>
2021-06-05 21:49:52 -0500

    added skeletal recipe gen

---
+++ src/main/java/mod/akkamaddi/cthon/datagen/Recipes.java
@@ -0,0 +1,25 @@
+package mod.akkamaddi.cthon.datagen;
+
+import mod.alexndr.simplecorelib.datagen.ISimpleConditionBuilder;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.RecipeProvider;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+
+public class Recipes extends RecipeProvider implements IConditionBuilder, ISimpleConditionBuilder
+{
+
+    public Recipes(DataGenerator p_i48262_1_)
+    {
+        super(p_i48262_1_);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public ICondition flag(String arg0)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
