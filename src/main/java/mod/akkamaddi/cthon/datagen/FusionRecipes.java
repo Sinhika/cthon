3f40834951adf9f677fd2f32a847ae6d8cb450d3
Sinhika <sinhika@republicofnewhome.org>
2021-06-06 15:57:57 -0500

    added cthon fusion recipe datagen

---
+++ src/main/java/mod/akkamaddi/cthon/datagen/FusionRecipes.java
@@ -0,0 +1,73 @@
+package mod.akkamaddi.cthon.datagen;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import mod.akkamaddi.cthon.Cthon;
+import mod.akkamaddi.cthon.config.CthonConfig;
+import mod.akkamaddi.cthon.init.ModItems;
+import mod.alexndr.fusion.api.datagen.FusionRecipeSetBuilder;
+import mod.alexndr.fusion.api.recipe.AbstractFusionRecipeProvider;
+import mod.alexndr.simplecorelib.datagen.ISimpleConditionBuilder;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.common.crafting.CompoundIngredient;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+
+public class FusionRecipes extends AbstractFusionRecipeProvider implements IConditionBuilder, ISimpleConditionBuilder
+{
+    private FusionRecipeSetBuilder fusionbuilder;
+
+    public FusionRecipes(DataGenerator generatorIn)
+    {
+        super(generatorIn);
+        fusionbuilder = new FusionRecipeSetBuilder(Cthon.MODID);
+    }
+
+    
+    @Override
+    protected void buildShapelessRecipes(Consumer<IFinishedRecipe> consumer)
+    {
+        registerCthonRecipes(consumer);
+        registerCthonRecycling(consumer);
+    }
+    
+    
+    protected void registerCthonRecipes(Consumer<IFinishedRecipe> consumer)
+    {
+        List<Ingredient> primary_inputs = new ArrayList<Ingredient>(2);
+        Ingredient[] catalysts = new Ingredient[3];
+        
+        primary_inputs.add(Ingredient.of(ModItems.cthon_mephitic_chunk.get()));
+        primary_inputs.add(Ingredient.of(Items.BLUE_ICE, Items.ICE, Items.PACKED_ICE));
+        catalysts[0] = null;
+        catalysts[1] = null;
+        catalysts[2] = Ingredient.of(Items.GOLD_NUGGET);
+        fusionbuilder.buildBasicAlloyRecipes(consumer, primary_inputs, catalysts, null,
+                null, ModItems.cthon_elutriated_chunk.get(), 0.25F, 600, null);
+    }
+    
+    
+    protected void registerCthonRecycling(Consumer<IFinishedRecipe> consumer)
+    {
+        fusionbuilder.buildFusionRecyclingRecipes(consumer,
+                Ingredient.of(ModItems.cthon_axe.get(), ModItems.cthon_boots.get(), ModItems.cthon_helmet.get(),
+                        ModItems.cthon_hoe.get(), ModItems.cthon_pickaxe.get(), ModItems.cthon_shovel.get(),
+                        ModItems.cthon_sword.get()),
+                Ingredient.of(ModItems.cthon_chestplate.get(), ModItems.cthon_leggings.get()),
+                Ingredient.of(Items.NETHERRACK), Ingredient.of(Items.GOLD_INGOT), 
+                ModItems.cthon_elutriated_chunk.get(), 20.0F, 600, flag("recycling_enabled"), "recycle_cthon_items");
+    }
+    
+    
+    @Override
+    public ICondition flag(String name)
+    {
+        return  impl_flag(Cthon.MODID, CthonConfig.INSTANCE, name);
+    }
+
+} // end class
