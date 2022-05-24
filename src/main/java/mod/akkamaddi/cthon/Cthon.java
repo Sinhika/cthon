1735dccd07c97a7eef31fc4ffd3c2c4eb897a0ad
Sinhika <sinhika@republicofnewhome.org>
2022-05-24 18:21:05 -0500

    remember to register features

--- src/main/java/mod/akkamaddi/cthon/Cthon.java
+++ src/main/java/mod/akkamaddi/cthon/Cthon.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 
 import mod.akkamaddi.cthon.config.ConfigHolder;
 import mod.akkamaddi.cthon.init.ModBlocks;
+import mod.akkamaddi.cthon.init.ModFeatures;
 import mod.akkamaddi.cthon.init.ModItems;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -36,6 +37,8 @@ public class Cthon
         
         ModBlocks.BLOCKS.register(modEventBus);
         ModItems.ITEMS.register(modEventBus);
+        ModFeatures.CONFIGURED_FEATURES.register(modEventBus);
+        ModFeatures.PLACED_FEATURES.register(modEventBus);
         
         // Register Configs
         modLoadingContext.registerConfig(ModConfig.Type.COMMON, ConfigHolder.SERVER_SPEC);
